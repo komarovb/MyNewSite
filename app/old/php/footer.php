eb30bd846cee7bb0cc2ad6d98a4eef8aaea7273c
komarovb <fisika80@gmail.com>
2015-07-31 10:47:11 +0200

    3 pages done

--- app/old/php/footer.php
+++
@@ -1,6 +0,0 @@
-<?php ?>
-<div class="line line2"></div>
-	<footer class="foot">
-
-		<p><span class="lock-icon"><a href="admin.html"><img src="img/login.png" class="foot-img"></a></span> © 2014 Это мой сайт пожалуйста не копируйте и не воруйте его!</p>
-	</footer>
